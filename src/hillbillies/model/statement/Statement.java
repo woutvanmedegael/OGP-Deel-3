7a9f7d6741ef22cfd7e889edbfca439c0adac9ce
Wout Van Medegael <woutvmdg@hotmail.com>
2016-05-06 15:00:18 +0200

    lkj

--- src/hillbillies/model/statement/Statement.java
+++ src/hillbillies/model/statement/Statement.java
@@ -5,23 +5,12 @@ package hillbillies.model.statement;
 
 
 
+import hillbillies.model.ContextWrapper;
 import hillbillies.model.IContainsSelected;
-import hillbillies.model.Position;
-import hillbillies.model.hillbilliesobject.unit.Unit;
-import hillbillies.model.world.World;
 import hillbillies.model.world.WorldException;
 
 public abstract class Statement implements IContainsSelected{
 	
-	private boolean executed=false;
+	public abstract Boolean execute(ContextWrapper context) throws WorldException ;
 	
-	public abstract Boolean execute(World world, Unit unit, Position selectedCube) throws WorldException ;
-	
-	public boolean hasBeenExecuted(){
-		return this.executed;
-	}
-	
-	public void setExecuted(boolean executed){
-	this.executed = executed;
-	}
 }
